ecc9fd6034b534d7190848d973eb04340d2cba7e
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2017-10-10 09:43:53 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -3,8 +3,8 @@ from flask import Flask, url_for, render_template, request
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
 @app.route("/")
-#def render_main():
-#    return render_template('index.html')
+def render_main():
+    return render_template('index.html')
 
 @app.route("/ftm")
 def render_response():
